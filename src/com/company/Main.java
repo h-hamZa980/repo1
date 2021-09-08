c7f31bd283bf6e2eb1a3af04ab8ecb4adde606b8
h-hamZa980 <muhammad.hamza0333@gmail.com>
2021-09-08 15:44:59 +0500

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+//import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	String message = "Hello World";
+     /* int [][] numbers=new int [2][3];
+        numbers[0][0]= 1;
+        System.out.println(Arrays.deepToString(numbers));*/
+
+        float number1 = 1.1F;
+        float number2 = 2.2F;
+        System.out.println(Math.floor(8.6));
+    }
+}
